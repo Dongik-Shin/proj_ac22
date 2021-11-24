5ffa43584b10dcea0bdfeb99a37e2f7cdbd6b1ed
dishin <dishin@jlkgroup.com>
2021-11-24 16:13:23 +0900

    주석 추가

--- v1/module/mongo.py
+++ v1/module/mongo.py
@@ -14,16 +14,34 @@ class Mongo:
         self.doc = None
 
     def get_col_list(self):
+        """ 컬렉션 리스트 리턴
 
+        Returns
+        -------
+        collection_names : 컬렉션 리스트 (list)
+        """
         return self.db.collection_names()
 
     def set_col(self, col):
+        """ 컬렉션 셋팅
 
+        Parameters : 
+        col : 컬렉션(str) 
+
+        Returns
+        -------
+        col : 컬렉션 (obj)
+        """
         self.col = self.db[col]
         return self.col
 
     def is_col(self):
+        """ 컬렉션이 정상 셋팅 되었는지 확인
 
+        Returns
+        -------
+        boolean 
+        """
         if self.col:
             if self.col.name in self.get_col_list():
                 return True
@@ -31,7 +49,15 @@ class Mongo:
         return False
 
     def insert_doc(self, post):
+        """ 다큐먼트 삽입
 
+        Parameters : 
+        post : json 데이터 (dict) 
+
+        Returns
+        -------
+        inserted_id : 삽입 된 아이디 (str)
+        """
         self.inserted_id = None
         if self.is_col():
             self.inserted_id = self.col.insert_one(post).inserted_id
@@ -39,6 +65,12 @@ class Mongo:
         return self.inserted_id
 
     def get_doc_one(self):
+        """ 다큐먼트 단일 조회
+
+        Returns
+        -------
+        doc : 조회 데이터 (dict) 
+        """
 
         self.doc = None
         if self.is_col():
@@ -47,7 +79,12 @@ class Mongo:
         return self.doc
 
     def get_doc(self, limit):
+        """ 다큐먼트 복수 조회
 
+        Returns
+        -------
+        doc : 조회 데이터 (list)) 
+        """
         results = self.col.find().sort("_id", DESCENDING).limit(limit)
 
         data_list = []
